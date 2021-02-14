3d26cd2a7d492fa737d370a1492e7d5f2fc0b79c
emaiolo <78452610+emaiolo@users.noreply.github.com>
2021-02-14 17:54:39 -0500

    Create DeviceInfoModel.java

---
+++ DeviceInfoModel.java
@@ -0,0 +1,23 @@
+package com.example.companionapp;
+
+public class DeviceInfoModel {
+
+    private String deviceName, deviceHardwareAddress;
+
+    public DeviceInfoModel(){}
+
+    public DeviceInfoModel(String deviceName, String deviceHardwareAddress){
+        this.deviceName = deviceName;
+        this.deviceHardwareAddress = deviceHardwareAddress;
+    }
+
+    public String getDeviceName(){
+        return deviceName;
+    }
+
+    public String getDevicehardwareAddress(){
+        return deviceHardwareAddress;
+    }
+
+
+}
